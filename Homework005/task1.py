6fb4cb9cbafe303b7a54abe61ba58250506c03f4
Alisa <Alica1200@gmail.com>
2022-09-19 18:29:37 +0300

    Add task 1

--- Homework005/task1.py
+++ Homework005/task1.py
@@ -7,3 +7,97 @@
 # Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
 # a) Добавьте игру против бота
 # b) Подумайте как наделить бота ""интеллектом""
+
+# а)
+from random import randint
+
+def input_dat(name):
+    x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
+    while x < 1 or x > 28:
+        x = int(input(f"{name}, введите корректное количество конфет: "))
+    return x
+
+def p_print(name, k, counter, value):
+    print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {candy} конфет.")
+
+player1 = input("Введите имя первого игрока: ")
+player2 = input("Введите имя второго игрока: ")
+candy = int(input("Введите количество конфет на столе: "))
+flag = randint(0, 2) #жеребьевка
+if flag:
+    print(f"Первый ходит {player1}")
+else:
+    print(f"Первый ходит {player2}")
+
+counter1 = 0
+counter2 = 0
+
+while candy > 28:
+    if flag:
+        k = input_dat(player1)
+        counter1 += k
+        candy -= k
+        flag = False
+        p_print(player1, k, counter1, candy)
+    else:
+        k = input_dat(player2)
+        counter2 += k
+        candy -= k
+        flag = True
+        p_print(player2, k, counter2, candy)
+
+if flag:
+    print(f"Выиграл {player1}")
+else:
+    print(f"Выиграл {player2}")
+
+
+# б)
+from random import randint
+def input_dat(name):
+    x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
+    while x < 1 or x > 28:
+        x = int(input(f"{name}, введите корректное количество конфет: "))
+    return x
+
+
+def p_print(name, k, counter, candy):
+    print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {candy} конфет.")
+
+
+def bot_calc(candy):
+    k = randint(1,29)
+    while candy-k <= 28 and candy > 29:
+        k = randint(1,29)
+    return k
+
+player1 = input("Введите имя первого игрока: ")
+player2 = "Bot"
+candy = int(input("Введите количество конфет на столе: "))
+flag = randint(0,2) 
+if flag:
+    print(f"Первый ходит {player1}")
+else:
+    print(f"Первый ходит {player2}")
+
+counter1 = 0
+counter2 = 0
+
+while candy > 28:
+    if flag:
+        k = input_dat(player1)
+        counter1 += k
+        candy -= k
+        flag = False
+        p_print(player1, k, counter1, candy)
+    else:
+        k = bot_calc(candy)
+        counter2 += k
+        candy -= k
+        flag = True
+        p_print(player2, k, counter2, candy)
+
+if flag:
+    print(f"Выиграл {player1}")
+else:
+    print(f"Выиграл {player2}")
