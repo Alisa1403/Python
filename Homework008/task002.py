5a88af17083091d48079022e48e04241da4135a4
Alisa <Alica1200@gmail.com>
2022-10-03 21:56:07 +0300

    Fixed task 2

--- Homework008/task002.py
+++ Homework008/task002.py
@@ -4,4 +4,11 @@
 # Input: 2 -> 3 4
 # Output: 4 3
 
-print(' '.join(input('Введите последовательность через пробел: ').split()[::-1]))
+
+def reverse(string):
+    if len(string) == 0:
+        return string
+    else:
+        return reverse(string[1:]) + string[0]
+a = str(input("Введите число: "))
+print(reverse(a))
